16601a453dc8fe72b6d5d69b05fbc823b62c1a8f
Sabbir Sarker <masabbir1257@gmail.com>
2023-08-10 02:38:43 +0600

    our expert section add slider

--- src/components/PagesSection/Home/OurServices/OurServiceSingleCart.jsx
+++ src/components/PagesSection/Home/OurServices/OurServiceSingleCart.jsx
@@ -2,6 +2,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import "@/styles/expert.modules.css";
+
 
 const ServiceSingleCard = ({singleCard}) => {
     const { id, serviceName, image,  about } = singleCard;
@@ -16,7 +18,6 @@ const ServiceSingleCard = ({singleCard}) => {
                  <h2 className=''>{serviceName}</h2>
                  <h2 className='font-semibold'>Experience: {about}</h2>
             </Link>
-
         </div>
         </div>
     );
