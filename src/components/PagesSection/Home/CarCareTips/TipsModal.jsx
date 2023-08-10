7ec9932a164c8fe49794453665f1813822fc0650
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-08-10 19:00:56 +0600

    color changes

--- src/components/PagesSection/Home/CarCareTips/TipsModal.jsx
+++ src/components/PagesSection/Home/CarCareTips/TipsModal.jsx
@@ -11,11 +11,11 @@ const NewsModal = ({ isOpen, closeModal, tip }) => {
       onRequestClose={closeModal}
       contentLabel="tip Modal"
     >
-      <div className="modal-content">
+      <div className="modal-content text-orange-600">
       <h1 >Car Care Tips</h1>
         <h2 className='mb-12 text-3xl'>{tip.title}</h2>
         <img src={tip.image} alt="news" />
-        <p className='mt-12 text-black-800'>News Details: <br />{tip.description}</p>
+        <p className='mt-12 text-black-800'>Tips Details: <br />{tip.description}</p>
         <p className='mt-12'>Authore Name: {tip.author}</p>
         <p>Publish date: {tip.date}</p>
         {/* Add more information */}
