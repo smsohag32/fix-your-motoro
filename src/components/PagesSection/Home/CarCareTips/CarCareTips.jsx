632fd36c947a1d512ec405fabda9e6d841ef57f4
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-08-10 16:28:42 +0600

    create car care tips  modal page

--- src/components/PagesSection/Home/CarCareTips/CarCareTips.jsx
+++ src/components/PagesSection/Home/CarCareTips/CarCareTips.jsx
@@ -1,5 +1,5 @@
 "use client"
-
+import TipsModal from './TipsModal'; 
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -10,26 +10,47 @@ import 'swiper/css/navigation';
 // import required modules
 import { Navigation } from 'swiper/modules';
 import "./tips.css";
-
+import "../RecentNews/news.css";
 
 import tipsData from './tips.json'; // Import the JSON data
 
 function CarCareTips() {
   const tips = tipsData.tips; // Access the tips array
+  const [selectedTip, setSelectedTip] = useState(null);
 
+
+  const openModal = (tip) => {
+    setSelectedTip(tip);
+  };
+
+  const closeModal = () => {
+    setSelectedTip(null);
+  };
 return (
   <><h1 className="">Car Care Tips</h1>
     <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+    <div className="container">
     {tips.map((tip) => (
           <SwiperSlide >
-            <div key={tip.id} className="card bg-slate-500 ">
+           <div key={tip.id} 
+          className="flex card" 
+          onClick={() => openModal(tip)}
+          >
             <img src={tip.image} alt="tips" />
-            <h2>{tip.title}</h2>
-            <p>{tip.description}</p>
+           <div className='ml-12  text-justify text-black-800'>
+           <h2>{tip.title}</h2>
+            <p className='mt-12 text-black-800'>News Details: <br />{tip.description}</p>
+            </div> 
           </div>
           </SwiperSlide>
         ))}
+         </div>
     </Swiper>
+    <TipsModal
+        isOpen={selectedTip !== null}
+        closeModal={closeModal}
+        tip={selectedTip || {}}
+      />
   </>
 );
 }
---
+++ src/components/PagesSection/Home/CarCareTips/TipsModal.jsx
@@ -0,0 +1,29 @@
+"use client";
+import React from 'react';
+import Modal from 'react-modal';
+import './tips.css';
+
+
+const NewsModal = ({ isOpen, closeModal, tip }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      contentLabel="tip Modal"
+    >
+      <div className="modal-content">
+      <h1 >Car Care Tips</h1>
+        <h2 className='mb-12 text-3xl'>{tip.title}</h2>
+        <img src={tip.image} alt="news" />
+        <p className='mt-12'>News Details: {tip.description}</p>
+        <p className='mt-12'>Authore Name: {tip.author}</p>
+        <p>Publish date: {tip.date}</p>
+        {/* Add more information */}
+      </div>
+      <button onClick={closeModal} 
+      className='text-white text-3xl mt-5 bg-red-900'>Close</button>
+    </Modal>
+  );
+};
+
+export default NewsModal;
