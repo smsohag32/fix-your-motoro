896610dedb623ab38e06cd53367d669a1de0e52d
SM Sohag <sohagsheik32@gmail.com>
2023-08-14 19:04:59 +0600

    update

---
+++ src/app/(dashboard)/dashboard/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dignissimos
+      rem architecto? Voluptatum deserunt modi praesentium, ea natus unde esse
+      fugiat blanditiis sint fuga nam neque fugit, reiciendis placeat alias!
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import "./globals.css";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,8 +9,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <div className={inter.className}>
-      <div className="">{children}</div>
-    </div>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
