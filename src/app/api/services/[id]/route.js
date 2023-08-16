f5a6805f8f8d3ad08a8f195e1e103357dd6dbd59
SM Sohag <sohagsheik32@gmail.com>
2023-08-16 20:44:00 +0600

    implemented some functionality

--- src/app/api/services/[id]/route.js
+++ src/app/api/services/[id]/route.js
@@ -1,21 +1,14 @@
 import { connectToDB } from "@/dbConfig/dbConfig";
 import services from "@/modules/services";
 
-export const GET = async (request) => {
+export const GET = async (request, { params }) => {
+  console.log(params.id);
   try {
     await connectToDB();
-    const queryParams = new URLSearchParams(request.url);
-    const category = queryParams.get("services_category");
-    const query = category ? { service_category: category } : {};
-    console.log(category);
-
-    const servicesData = await services.find(query);
-
-    if (!servicesData)
-      return new Response("services not found", { status: 404 });
-
-    return new Response(JSON.stringify(servicesData), { status: 200 });
+    const service = await services.findById(params.id);
+    return new Response(JSON.stringify(service), { status: 200 });
   } catch (error) {
-    return new Response("Failed to fetch data", { status: 500 });
+    console.log(error);
+    return new Response("failed", { status: 500 });
   }
 };
