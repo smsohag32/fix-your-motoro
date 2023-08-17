4cc03dbb74f745806a57f4e73a6fa89c469da9c4
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-08-17 09:41:27 +0600

    work with some error

--- src/components/PagesSection/Home/CarCareTips/CarCareTips.jsx
+++ src/components/PagesSection/Home/CarCareTips/CarCareTips.jsx
@@ -1,59 +1,7 @@
 import React from "react";
-import TipsModal from "./TipsModal";
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-// import required modules
-import { Navigation } from 'swiper/modules';
-import "./tips.modules.css";
-import "../RecentNews/news.css";
-
-import tipsData from './tips.json'; // Import the JSON data
-import Image from 'next/image';
-import Swiper from "swiper";
-import { SwiperSlide } from "swiper/react";
-
-function CarCareTips() {
-  const tips = tipsData.tips; // Access the tips array
-  const [selectedTip, setSelectedTip] = useState(null);
-
-
-  const openModal = (tip) => {
-    setSelectedTip(tip);
-  };
-
-  const closeModal = () => {
-    setSelectedTip(null);
-  };
-return (
-  <><h1 className=" ">Car Care Tips</h1>
-    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-    <div className="container">
-    {tips.map((tip) => (
-          <SwiperSlide key={tip.id} >
-           <div key={tip.id} 
-          className="flex card" 
-          onClick={() => openModal(tip)}
-          >
-           <Image src={tip.image} width={500} height={300} alt="tips" style={{ width: '50%' }}/>
-           {/* <img src={tip.image} alt="tips" /> */}
-           <div className='ml-12  text-justify'>
-           <h2>{tip.title}</h2>
-            <p className='mt-12'>Tips Details: <br />{tip.description}</p>
-            </div> 
-          </div>
-          </SwiperSlide>
-        ))}
-         </div>
-    </Swiper>
-    <TipsModal
-        isOpen={selectedTip !== null}
-        closeModal={closeModal}
-        tip={selectedTip || {}}
-      />
-  </>
-);
-}
+const CarCareTips = () => {
+  return <div></div>;
+};
 
 export default CarCareTips;
--- src/components/PagesSection/WorkShops/Workshop/WorkShops.jsx
+++ src/components/PagesSection/WorkShops/Workshop/WorkShops.jsx
@@ -1,20 +1,88 @@
-import PageTitle from "@/components/Shared/PageTitle/PageTitle";
-import SectionTitle from "@/components/Shared/SectionTitle/SectionTitle";
-import React from "react";
+// import PageTitle from "@/components/Shared/PageTitle/PageTitle";
+// import SectionTitle from "@/components/Shared/SectionTitle/SectionTitle";
+// import React from "react";
 
-const WorkShops = () => {
+// const WorkShops = () => {
+//   return (
+//     <div>
+//       <PageTitle title="Workshop" subTitle={"You are treble now!"} />
+//       <div className="default-container">
+//         <SectionTitle
+//           title={"Our Featured Gradge"}
+//           subTitle={"to be like you"}
+//         />
+//         <div></div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default WorkShops;
+
+"use client";
+import React, { useState } from "react";
+import WorkShop from "../../../../utils/data/workShopData";
+import SectionTitle from "@/components/Shared/SectionTitle/SectionTitle";
+import Image from "next/image";
+import SingleWorkshop from "../SingleWorkShop/SingleWorkshop";
+function WorkShops() {
+  const workshopsData = WorkShop.workshops; // Access the workshops array
+  const [selectedWorkshops, setSelectedWorkshops] = useState(null);
+  console.log(workshopsData);
+  const [garage, setGarage] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("")
+  //     .then((res) => res.json())
+  //     .then((data) => setBlog(data))
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //     });
+  // }, []);
+
+  const openModal = (workshops) => {
+    setSelectedWorkshops(workshops);
+  };
+
+  const closeModal = () => {
+    setSelectedWorkshops(null);
+  };
+
   return (
-    <div>
-      <PageTitle title="Workshop" subTitle={"You are treble now!"} />
-      <div className="default-container">
-        <SectionTitle
-          title={"Our Featured Gradge"}
-          subTitle={"to be like you"}
-        />
-        <div></div>
+    <div className="default-container py-12">
+      <SectionTitle
+        title="Special Workshop"
+        subTitle="Recent motor servicing Workshop"
+      />
+      <div className="grid grid-cols-1 my-12 md:grid-cols-3 gap-6">
+        {
+        workshopsData.map((workshop) => (
+          <div
+            key={workshop.id}
+            className="cursor-pointer flex flex-col primary-shadow hover:scale-95 duration-500 pb-5 transition-all transform items-center"
+            onClick={() => openModal(workshop)}
+          >
+
+            <Image
+              src={workshop.image}
+              alt="news"
+              width={280}
+              className="w-full h-44 object-cover"
+              height={280}
+            />
+            <h2 className="text-lg px-3 font-semibold tracking-wider mt-4 md:text-xl">
+              {workshop.name}
+            </h2>
+          </div>
+        ))}
       </div>
+      <SingleWorkshop
+        isOpen={selectedWorkshops !== null}
+        closeModal={closeModal}
+        workshop={selectedWorkshops || {}}
+      />
     </div>
   );
-};
+}
 
 export default WorkShops;
