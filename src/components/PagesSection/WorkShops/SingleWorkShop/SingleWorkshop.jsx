9546504a96df60d35311755c6255a28b98a7264e
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-08-17 14:44:03 +0600

    data receive from backend and create more powerful design

--- src/components/PagesSection/WorkShops/SingleWorkShop/SingleWorkshop.jsx
+++ src/components/PagesSection/WorkShops/SingleWorkShop/SingleWorkshop.jsx
@@ -1,34 +1,51 @@
-// import React from "react";
-
-// const SingleWorkshop = () => {
-//   return <div></div>;
-// };
-
-// export default SingleWorkshop;
-"use client";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import Modal from "react-modal";
 
-const SingleWorkshop = ({ isOpen, closeModal, workshop }) => {
+const SingleWorkshop = (props) => {
+  // console.log(props.service);
+  const { name, _id, image,description } =
+    props.workshopsData;
   return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={closeModal}
-      contentLabel="workshop Modal"
-    >
-      <div className="modal-content mt--20">
-        <h1>Motor Servicing News</h1>
-        <h2 className="mb-12 text-3xl">{workshop.name}</h2>
-        <Image width={290} height={390} src={workshop.image} alt="news" />
-        <p className="mt-12">workshop Details: {workshop.description}</p>
-        <p className="mt-12">Address: {workshop.address}</p>
-        <p>address: {workshop.email}</p>
+    <div>
+      <div className="flex items-center justify-center bg-gray-50">
+        <div className="">
+          <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div className="h-72 w-96">
+              <Image
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src={image}
+                alt=""
+                width={384}
+                height={288}
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-4 pb-12 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 className="font-serif text-3xl font-bold text-white">
+                {name}
+              </h1>
+              <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                {description}
+              </p>
+              <Link href={`/services/${_id}`}>
+                <button className="rounded-full bg-white py-2 px-3.5 font-semibold text-sm capitalize text-black  shadow shadow-black/60">
+                  View Details
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* <div class="fixed bottom-16">
+          <p class="font-com text-2xl font-semibold text-white">
+            All Images are from{" "}
+            <a href="https://unsplash.com" class="text-blue-500">
+              Unsplash.com
+            </a>
+          </p>
+        </div> */}
       </div>
-      <button onClick={closeModal} className="primary-btn">
-        Close
-      </button>
-    </Modal>
+    </div>
   );
 };
 
--- src/components/PagesSection/WorkShops/Workshop/WorkShops.jsx
+++ src/components/PagesSection/WorkShops/Workshop/WorkShops.jsx
@@ -1,62 +1,102 @@
+// "use client";
+// import React, { useState } from "react";
+// import WorkShop from "./workshopData.json";
+// import Image from "next/image";
+// import SingleWorkshop from "../SingleWorkShop/SingleWorkshop";
+// function WorkShops() {
+//   const workshops = WorkShop.workshops; // Access the workshops array
+//   const [selectedWorkshops, setSelectedWorkshops] = useState(null);
+//   console.log(workshops);
+//   const [garage, setGarage] = useState([]);
+
+//   // useEffect(() => {
+//   //   fetch("")
+//   //     .then((res) => res.json())
+//   //     .then((data) => setBlog(data))
+//   //     .catch((error) => {
+//   //       console.error("Error fetching data:", error);
+//   //     });
+//   // }, []);
+
+//   const openModal = (workshops) => {
+//     setSelectedWorkshops(workshops);
+//   };
+
+//   const closeModal = () => {
+//     setSelectedWorkshops(null);
+//   };
+
+//   return (
+//     <div className="default-container py-12  mt-12">
+//       <div className="grid grid-cols-1 my-12 md:grid-cols-3 gap-6">
+//         {
+//         workshops.map((workshop) => (
+//           <div
+//             key={workshop.id}
+//             className="cursor-pointer flex flex-col primary-shadow hover:scale-95 duration-500 pb-5 transition-all transform items-center"
+//             onClick={() => openModal(workshop)}
+//           >
+
+//             <Image
+//               src={workshop.image}
+//               alt="news"
+//               width={280}
+//               className="w-full h-44 object-cover"
+//               height={280}
+//             />
+//             <h2 className="text-lg px-3 font-semibold tracking-wider mt-4 md:text-xl">
+//               {workshop.name}
+//             </h2>
+//           </div>
+//         ))}
+//       </div>
+//       <SingleWorkshop
+//         isOpen={selectedWorkshops !== null}
+//         closeModal={closeModal}
+//         workshop={selectedWorkshops || {}}
+//       />
+//     </div>
+//   );
+// }
+
+// export default WorkShops;
 "use client";
-import React, { useState } from "react";
-import WorkShop from "./workshopData.json";
-import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import WorkshopFilter from "../WorkshopFilter/WorkshopFilter";
 import SingleWorkshop from "../SingleWorkShop/SingleWorkshop";
-function WorkShops() {
-  const workshops = WorkShop.workshops; // Access the workshops array
-  const [selectedWorkshops, setSelectedWorkshops] = useState(null);
-  console.log(workshops);
-  const [garage, setGarage] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("")
-  //     .then((res) => res.json())
-  //     .then((data) => setBlog(data))
-  //     .catch((error) => {
-  //       console.error("Error fetching data:", error);
-  //     });
-  // }, []);
+const WorkShops = () => {
+  const [workshopsData, setWorkshopsData] = useState([]);
 
-  const openModal = (workshops) => {
-    setSelectedWorkshops(workshops);
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/api/workshops");
+      const data = await response.json();
+      setWorkshopsData(data);
+    } catch (error) {
+      console.error("Error fetching JSON data:", error);
+    }
   };
 
-  const closeModal = () => {
-    setSelectedWorkshops(null);
-  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <div className="default-container py-12  mt-12">
-      <div className="grid grid-cols-1 my-12 md:grid-cols-3 gap-6">
-        {
-        workshops.map((workshop) => (
-          <div
-            key={workshop.id}
-            className="cursor-pointer flex flex-col primary-shadow hover:scale-95 duration-500 pb-5 transition-all transform items-center"
-            onClick={() => openModal(workshop)}
-          >
-
-            <Image
-              src={workshop.image}
-              alt="news"
-              width={280}
-              className="w-full h-44 object-cover"
-              height={280}
-            />
-            <h2 className="text-lg px-3 font-semibold tracking-wider mt-4 md:text-xl">
-              {workshop.name}
-            </h2>
-          </div>
+    <>
+      <div className="my-8 text-right">
+        <WorkshopFilter></WorkshopFilter>
+      </div>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+        {workshopsData.map((workshop) => (
+          <SingleWorkshop
+            key={workshop.workshop_id}
+            workshopsData={workshop}
+          ></SingleWorkshop>
         ))}
       </div>
-      <SingleWorkshop
-        isOpen={selectedWorkshops !== null}
-        closeModal={closeModal}
-        workshop={selectedWorkshops || {}}
-      />
-    </div>
+    </>
   );
-}
+};
 
 export default WorkShops;
