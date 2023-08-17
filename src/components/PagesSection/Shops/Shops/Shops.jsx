bbbd01b3a1fde9a7162add8380a6a121151069fe
Tarek Aziz <mdaziz3289@gmail.com>
2023-08-18 01:25:52 +0600

    filter added

--- src/components/PagesSection/Shops/Shops/Shops.jsx
+++ src/components/PagesSection/Shops/Shops/Shops.jsx
@@ -2,13 +2,20 @@
 import { AiFillHeart } from "react-icons/ai";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import StarRating from "../../Home/SuccessReviews/StarRating";
+import toast, { Toaster } from "react-hot-toast";
 
 const Shops = () => {
-  const router= useRouter()
+  // const router = useRouter();
   const [shopData, setShopData] = useState([]);
   const [like, setLike] = useState(true);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+
+  const notify = () => toast("Coming Soon...");
 
   useEffect(() => {
     fetch("/data/shop.json")
@@ -19,11 +26,41 @@ const Shops = () => {
       });
   }, []);
 
+  const filteredShopData = shopData.filter(
+    (item) =>
+      item.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
+      (minPrice === "" || item.price >= parseFloat(minPrice)) &&
+      (maxPrice === "" || item.price <= parseFloat(maxPrice))
+  );
+
   return (
-    <div className="default-container">
-      <div className="max-w-4xl mx-auto px-4">
+    <div className="default-container py-12">
+      <div className=" px-4">
+        <div className="flex mb-4">
+          <input
+            type="number"
+            placeholder="Min Price"
+            value={minPrice}
+            onChange={(e) => setMinPrice(e.target.value)}
+            className="border p-2 mr-2"
+          />
+          <input
+            type="number"
+            placeholder="Max Price"
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(e.target.value)}
+            className="border p-2"
+          />
+        </div>
+        <input
+          type="text"
+          placeholder="Search products"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="border p-2 mb-4 w-full"
+        />
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {shopData.map((item) => (
+          {filteredShopData.map((item) => (
             <div
               key={item.id}
               className="bg-white rounded-lg shadow-md p-5 sm:p-6 relative transition hover:bg-red-50"
@@ -61,16 +98,17 @@ const Shops = () => {
                   <p className="ml-1">{item.rating.toFixed(1)}</p>
                 </div>
               </div>
-              <h3 className="text-lg font-semibold my-2">{item.title}</h3>
+              <h3 className="text-lg font-semibold my-2">{item.name}</h3>
 
               <div className="flex justify-between items-center my-2">
-                <button
+                {/* <button
                   onClick={() => router.push(`/shops/${item.id}`)}
                   className="primary-btn"
                 >
                   View Details
-                </button>
-
+                </button> */}
+                <button onClick={notify}>Add to Card</button>
+                <Toaster />
                 <p className="text-gray-700">${item.price.toFixed(2)}</p>
               </div>
             </div>
