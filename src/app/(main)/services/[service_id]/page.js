cb6548f75a9b2abe0bbd3f8dc9689eb96935ac33
trtonmoy <tr.tonmoy0110.trt@gmal.com>
2023-08-17 14:29:39 +0600

    single service desinging

--- src/app/(main)/services/[service_id]/page.js
+++ src/app/(main)/services/[service_id]/page.js
@@ -1,3 +1,4 @@
+import PageTitle from "@/components/Shared/PageTitle/PageTitle";
 import loadSingleService from "@/utils/data/fetchData/loadSingleService";
 import Image from "next/image";
 import React from "react";
@@ -16,39 +17,54 @@ const ServicePage = async ({ params }) => {
     warranty,
   } = await loadSingleService(params.service_id);
   return (
-    <div className="mt-32">
+    <div className="mt-32 default-container">
+      <PageTitle
+        title={service_name}
+        subTitle={service_description}
+      ></PageTitle>
       <div>
         {/* Service Area */}
-        <div>
+        <div className="lg:flex justify-content items-center gap-10">
           <figure>
             <Image
               src={service_image}
               alt={service_name}
-              width={850}
-              height={450}
+              width={550}
+              height={350}
             />
-            <figcaption> {service_name} </figcaption>
+            <figcaption className="text-sm font-thin">
+              {service_name}
+            </figcaption>
           </figure>
-        </div>
-        <div>
-          <h3> {service_name} </h3>
-          <p> {service_description} </p>
-          <p> {service_duration} </p>
-          <p> {service_price} </p>
-          <p> {benefits} </p>
-          <p> {warranty} </p>
-        </div>
-        <div>
-          {customer_reviews.map((review, idx) => (
-            <p key={idx}> {review} </p>
-          ))}
+          <div>
+            <h3 className="text-2xl text-slate-950 font-semibold tracking-wide">
+              {service_name}
+            </h3>
+            <p className="text-slate-600 text-xl font-medium">
+              {service_description}
+            </p>
+            <p className="text-lg font-medium primary-text">
+              {service_duration}
+            </p>
+            <p className="text-slate-600 font-lg tracking-wide">
+              {" "}
+              {service_price}{" "}
+            </p>
+            <p className="text-slate-600 font-lg tracking-wide"> {benefits} </p>
+            <p className="text-slate-600 font-lg tracking-wide"> {warranty} </p>
+            <div>
+              {customer_reviews.map((review, idx) => (
+                <p key={idx}> {review} </p>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
       <div>
         {/* Workshop Area */}
         <div>
           <figure>
-          <Image
+            <Image
               src={workshop_image}
               alt="Workshop"
               width={850}
@@ -57,9 +73,9 @@ const ServicePage = async ({ params }) => {
           </figure>
         </div>
         <div>
-          <h2>  </h2>
+          <h2> </h2>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
