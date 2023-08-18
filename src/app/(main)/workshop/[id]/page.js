1a607430291567a24f3a81f9c9710d444839e3cd
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-08-18 10:47:47 +0600

    create the details page about workshop

--- src/app/(main)/workshop/[id]/page.js
+++ src/app/(main)/workshop/[id]/page.js
@@ -1,10 +1,12 @@
 import SingleWorkshop from "@/components/PagesSection/WorkShops/SingleWorkShop/SingleWorkshop";
+import WorkshopDetails from "@/components/PagesSection/WorkShops/WorkshopDetails/WorkshopDetails";
 import React from "react";
 
 const WorkShops = () => {
   return (
     <div>
-      <SingleWorkshop />
+        <WorkshopDetails  />
+   
     </div>
   );
 };
--- src/components/PagesSection/WorkShops/WorkshopDetails/WorkshopDetails.jsx
+++ src/components/PagesSection/WorkShops/WorkshopDetails/WorkshopDetails.jsx
@@ -1,29 +1,27 @@
-// import React from "react";
-
-// const SingleWorkshop = () => {
-//   return <div></div>;
-// };
-
-// export default SingleWorkshop;
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
 
-const SingleWorkshop = ({ isOpen, closeModal, workshop }) => {
+const  WorkshopDetails = ({ isOpen, closeModal, workshop }) => {
+  console.log(workshop);
+
+  const {name,image,description,email,address } =workshop
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={closeModal}
       contentLabel="workshop Modal"
     >
-      <div className="modal-content mt--20">
-        <h1>Motor Servicing News</h1>
-        <h2 className="mb-12 text-3xl">{workshop.name}</h2>
-        <Image width={290} height={390} src={workshop.image} alt="news" />
-        <p className="mt-12">workshop Details: {workshop.description}</p>
-        <p className="mt-12">Address: {workshop.address}</p>
-        <p>address: {workshop.email}</p>
+      <div className="modal-content mt-20">
+        
+       
+        <Image width={290}height={390} src={image} alt="workshop" />
+        <h2 className="mb-12 text-3xl">{name}</h2>
+        <p className="mt-12">workshop Details: {description}</p>
+        <p className="mt-12">Address: {address}</p>
+        <p>Email: {email}</p>
+        {/* <p>Email: {products[0].title}</p> */}
       </div>
       <button onClick={closeModal} className="primary-btn">
         Close
@@ -32,4 +30,4 @@ const SingleWorkshop = ({ isOpen, closeModal, workshop }) => {
   );
 };
 
-export default SingleWorkshop;
+export default WorkshopDetails;
