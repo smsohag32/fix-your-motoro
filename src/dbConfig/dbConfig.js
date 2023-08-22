0bc6229824b52466ce3b75e33acd4eea11382558
SM Sohag <sohagsheik32@gmail.com>
2023-08-22 12:37:39 +0600

    update

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-let isConnected = false; // track the connection
+let isConnected = false; // track the connectio
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
