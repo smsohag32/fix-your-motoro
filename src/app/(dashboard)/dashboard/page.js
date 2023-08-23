69728e6b4956913ece767a830df2d4e6b090259e
trtonmoy <tr.tonmoy0110.trt@gmal.com>
2023-08-23 21:00:27 +0600

    modified

--- src/app/(dashboard)/dashboard/page.js
+++ src/app/(dashboard)/dashboard/page.js
@@ -24,14 +24,6 @@ const OverviewDashboard = () => {
             </p>
             <p>Feel free to customize this dashboard to suit your needs.</p>
           </div>
-          <div className="bg-white rounded-lg shadow-md p-6">
-            <h2 className="text-xl font-semibold mb-4">Dashboard Overview</h2>
-            <p>
-              Welcome to your workshop dashboard. Here, you can manage various
-              aspects of your workshop.
-            </p>
-            <p>Feel free to customize this dashboard to suit your needs.</p>
-          </div>
         </main>
       </div>
     </div>
