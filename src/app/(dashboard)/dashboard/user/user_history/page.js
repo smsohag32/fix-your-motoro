906b4aa7df9140555f5746f0bcd6de292b54fcfd
sabbir1257 <masabbir1257@gmail.com>
2023-08-24 01:57:22 +0600

    user link section push

---
+++ src/app/(dashboard)/dashboard/user/user_history/page.js
@@ -0,0 +1,12 @@
+import UserPayments from '@/components/dashboard/UserDashboard/UserPayments';
+import React from 'react';
+
+const UserHostoryPage = () => {
+    return (
+        <div>
+            <UserPayments/>
+        </div>
+    );
+};
+
+export default UserHostoryPage;
--- src/components/dashboard/UserDashboard/UserSarvice.jsx
+++ src/components/dashboard/UserDashboard/UserSarvice.jsx
@@ -1,91 +1,94 @@
-import PageTitle from '@/components/Shared/PageTitle/PageTitle'
-import React, { Component } from 'react'
+"use client";
+import DashboardTitle from "@/components/Shared/DashboardTitle/DashboardTitle";
+import React, { Component } from "react";
 
 export class UserSarvice extends Component {
   render() {
     return (
-      <div className="relative overflow-x-auto md:mt-40">
-         <PageTitle
-        title="Our Service"
-        subTitle="Welcome to the Service"
-      />
-            <table className="md:w-[80%] w-full mx-auto text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3 rounded-l-lg">
-                            Product name
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Qty
-                        </th>
-                        <th scope="col" className="px-6 py-3 rounded-r-lg">
-                            Price
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            View Details
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                          Rating
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className="bg-white dark:bg-gray-800">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17
-                        </th>
-                        <td className="px-6 py-4">
-                            1
-                        </td>
-                        <td className="px-6 py-4">
-                            $2999
-                        </td>
-                        <td className="px-6 py-4">
-                            <button className="rounded-lg primary-btn">View</button>
-                        </td>
-                        <td className="px-6 py-4">
-                            <button className="font-medium text-red-600 dark:text-blue-500 hover:underline">Rating</button>
-                        </td>
-                    </tr>
-                    <tr className="bg-white dark:bg-gray-800">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Microsoft Surface Pro
-                        </th>
-                        <td className="px-6 py-4">
-                            1
-                        </td>
-                        <td className="px-6 py-4">
-                            $1999
-                        </td>
-                        <td className="px-6 py-4">
-                            <button className="rounded-lg primary-btn">View</button>
-                        </td>
-                        <td className="px-6 py-4">
-                            <button className="font-medium text-red-600 dark:text-blue-500 hover:underline">Delete</button>
-                        </td>
-                    </tr>
-                    <tr className="bg-white dark:bg-gray-800">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Magic Mouse 2
-                        </th>
-                        <td className="px-6 py-4">
-                            1
-                        </td>
-                        <td className="px-6 py-4">
-                            $99
-                        </td>
-                        <td className="px-6 py-4">
-                            <button className="rounded-lg primary-btn">View</button>
-                        </td>
-                        <td className="px-6 py-4">
-                            <button className="font-medium text-red-600 dark:text-blue-500 hover:underline">Rating</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+      <>
+        <DashboardTitle title="Our Service" subTitle="Welcome to the Service" />
+        <div className="relative mt-10 overflow-x-auto">
+          <table className="md:w-[80%] w-full mx-auto text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3 rounded-l-lg">
+                  Product name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Qty
+                </th>
+                <th scope="col" className="px-6 py-3 rounded-r-lg">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  View Details
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Rating
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-white dark:bg-gray-800">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  Apple MacBook Pro 17
+                </th>
+                <td className="px-6 py-4">1</td>
+                <td className="px-6 py-4">$2999</td>
+                <td className="px-6 py-4">
+                  <button className="rounded-lg primary-btn">View</button>
+                </td>
+                <td className="px-6 py-4">
+                  <button className="font-medium text-red-600 dark:text-blue-500 hover:underline">
+                    Rating
+                  </button>
+                </td>
+              </tr>
+              <tr className="bg-white dark:bg-gray-800">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  Microsoft Surface Pro
+                </th>
+                <td className="px-6 py-4">1</td>
+                <td className="px-6 py-4">$1999</td>
+                <td className="px-6 py-4">
+                  <button className="rounded-lg primary-btn">View</button>
+                </td>
+                <td className="px-6 py-4">
+                  <button className="font-medium text-red-600 dark:text-blue-500 hover:underline">
+                      Rating
+                  </button>
+                </td>
+              </tr>
+              <tr className="bg-white dark:bg-gray-800">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  Magic Mouse 2
+                </th>
+                <td className="px-6 py-4">1</td>
+                <td className="px-6 py-4">$99</td>
+                <td className="px-6 py-4">
+                  <button className="rounded-lg primary-btn">View</button>
+                </td>
+                <td className="px-6 py-4">
+                  <button className="font-medium text-red-600 dark:text-blue-500 hover:underline">
+                    Rating
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-    )
+      </>
+    );
   }
 }
 
-export default UserSarvice
+export default UserSarvice;
