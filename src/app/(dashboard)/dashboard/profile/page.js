f4f45c6bb333b884363452e789338ead39558975
trtonmoy <tr.tonmoy0110.trt@gmal.com>
2023-08-23 18:36:40 +0600

    modified

--- src/app/(dashboard)/dashboard/profile/page.js
+++ src/app/(dashboard)/dashboard/profile/page.js
@@ -2,6 +2,19 @@ const page = () => {
   return (
     <div>
       <h3>i am from profile</h3>
+      <h3>i am from profile</h3>
+      <h3>i am from profile</h3>
+      <h3>i am from profile</h3>
+      <h3>i am from profile</h3>
+      <h3>i am from profile</h3>
+      <p>helo......................</p>
+      <p>helo......................</p>
+      <p>helo......................</p>
+      <p>helo......................</p>
+      <p>helo......................</p>
+      <p>helo......................</p>
+      <p>helo......................</p>
+      <p>helo......................</p>
     </div>
   );
 };
