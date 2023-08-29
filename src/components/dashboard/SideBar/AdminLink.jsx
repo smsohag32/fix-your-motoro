fb046d929c532a1f791f0472082fe52d376c8c8e
Tarek Aziz <mdaziz3289@gmail.com>
2023-08-30 00:03:58 +0600

    update

--- src/components/dashboard/SideBar/AdminLink.jsx
+++ src/components/dashboard/SideBar/AdminLink.jsx
@@ -13,52 +13,74 @@ const  AdminLInk = () => {
 
   return (
     <ul className="pb-4 my-4 border-b border-gray-100">
-        <li className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-200 group hover:shadow-lg">
-            <MdDashboard className="w-8 h-8  group-hover:text-[#f02801]" />
-            <h3 className="text-base font-semibold text-gray-800 group-hover:text-[#f02801] ">
-                <NavLink
-              exact={'/dashboard' == "/dashboard"}
-              activeClassName={"text-[#f02801]"}
-              href={'/dashboard'}
-            >
-             Summary
-            </NavLink>
-            </h3>
-        </li>
-       
-        {/* add to card section  */}
-        <li className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-200 group hover:shadow-lg">
-            <BiCreditCard className="w-8 h-8  group-hover:text-[#f02801]" />
-            <h3 className="text-base font-semibold text-gray-800 group-hover:text-[#f02801] ">
-                <NavLink exact={'/dashboard/user/user_service' == "/dashboard"}
-              activeClassName={"text-[#f02801]"} href="/dashboard/user/upcomming_appointment">Manage Users</NavLink>
-            </h3>
-        </li>
-        
-        {/* Support section  */}
-        <li className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-200 group hover:shadow-lg">
-            <FaHistory className="w-8 h-8 text-gray-600 group-hover:text-[#f02801]" />
-            <h3 className="text-base font-semibold text-gray-800 group-hover:text-[#f02801] ">
-                <Link href="/dashboard/user/user_history">Manage Workshops</Link>
-            </h3>
-        </li>
+      <li className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-200 group hover:shadow-lg">
+        <MdDashboard className="w-8 h-8  group-hover:text-[#f02801]" />
+        <h3 className="text-base font-semibold text-gray-800 group-hover:text-[#f02801] ">
+          <NavLink
+            exact={"/dashboard" == "/dashboard"}
+            activeClassName={"text-[#f02801]"}
+            href={"/dashboard"}
+          >
+            Summary
+          </NavLink>
+        </h3>
+      </li>
 
-        <li className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-200 group hover:shadow-lg">
-            <FaUsers className="w-8 h-8  group-hover:text-[#f02801] " />
-            <h3 className="text-base font-semibold text-gray-800 group-hover:text-[#f02801] ">
-                <NavLink   exact={'/dashboard/comuinity' == "/dashboard"}
-              activeClassName={"text-[#f02801]"} href="/dashboard/user/user_service">Community</NavLink>
-            </h3>
-        </li>
-        <li className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-200 group hover:shadow-lg">
-            <FaUsers className="w-8 h-8  group-hover:text-[#f02801] " />
-            <h3 className="text-base font-semibold text-gray-800 group-hover:text-[#f02801] ">
-                <NavLink   exact={'/dashboard/comuinity' == "/dashboard"}
-              activeClassName={"text-[#f02801]"} href="/dashboard/admin/profile">Account</NavLink>
-            </h3>
-        </li>
-        </ul>
-    );
+      {/* add to card section  */}
+      <li className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-200 group hover:shadow-lg">
+        <BiCreditCard className="w-8 h-8  group-hover:text-[#f02801]" />
+        <h3 className="text-base font-semibold text-gray-800 group-hover:text-[#f02801] ">
+          <NavLink
+            exact={"/dashboard/user/user_service" == "/dashboard"}
+            activeClassName={"text-[#f02801]"}
+            href="/dashboard/user/upcomming_appointment"
+          >
+            Manage Users
+          </NavLink>
+        </h3>
+      </li>
+
+      {/* Support section  */}
+      <li className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-200 group hover:shadow-lg">
+        <FaHistory className="w-8 h-8 text-gray-600 group-hover:text-[#f02801]" />
+        <h3 className="text-base font-semibold text-gray-800 group-hover:text-[#f02801] ">
+          <NavLink
+            exact={"/dashboard/user/user_service" == "/dashboard"}
+            activeClassName={"text-[#f02801]"}
+            href="/dashboard/user/user_history"
+          >
+            Manage Workshops
+          </NavLink>
+          {/* <Link href="/dashboard/user/user_history">Manage Workshops</Link> */}
+        </h3>
+      </li>
+
+      <li className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-200 group hover:shadow-lg">
+        <FaUsers className="w-8 h-8  group-hover:text-[#f02801] " />
+        <h3 className="text-base font-semibold text-gray-800 group-hover:text-[#f02801] ">
+          <NavLink
+            exact={"/dashboard/comuinity" == "/dashboard"}
+            activeClassName={"text-[#f02801]"}
+            href="/dashboard/user/user_service"
+          >
+            Community
+          </NavLink>
+        </h3>
+      </li>
+      <li className="flex items-center justify-start gap-4 p-2 pl-5 m-auto mb-2 rounded-md cursor-pointer hover:bg-gray-200 group hover:shadow-lg">
+        <FaUsers className="w-8 h-8  group-hover:text-[#f02801] " />
+        <h3 className="text-base font-semibold text-gray-800 group-hover:text-[#f02801] ">
+          <NavLink
+            exact={"/dashboard/comuinity" == "/dashboard"}
+            activeClassName={"text-[#f02801]"}
+            href="/dashboard/admin/profile"
+          >
+            Account
+          </NavLink>
+        </h3>
+      </li>
+    </ul>
+  );
 }
 
 export default AdminLInk;
