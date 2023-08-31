a056a0a56b421e3b4c0847144665bc97342b5610
smsohag32 <sohagsheik32@gmail.com>
2023-08-31 13:50:53 +0600

    update

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -2,8 +2,7 @@ import Sidebar from "@/components/dashboard/SideBar/Sidebar";
 import DashboardTopBar from "@/components/dashboard/Topbar/DashboardTopBar";
 import SearchProvider from "@/providers/SearchProvider";
 import PrivateRote from "@/utils/PrivateRote";
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata = {
   title: "FYM",
@@ -14,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <PrivateRote>
       <SearchProvider>
-      <div className={inter.className}>
+      <div>
         <div className="relative min-h-screen md:flex">
           <Sidebar />
           <div className="flex-1 md:ml-72 bg-slate-200">
