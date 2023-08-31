9253202e565073e54d09604b8c15114040fefdcd
trtonmoy <tr.tonmoy0110.trt@gmal.com>
2023-08-31 18:27:23 +0600

    making the admin chart

---
+++ src/components/dashboard/AdminDashboard/SummeryChart/SummeryChart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SummeryChart = () => {
+  return (
+    <div>
+      <p>This will contain the summery chart on admin dashboard</p>
+    </div>
+  );
+};
+
+export default SummeryChart;
