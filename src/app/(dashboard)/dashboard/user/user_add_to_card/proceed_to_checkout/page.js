48eafd118c1408a4861cb49cc06b7633437207ea
sabbir1257 <masabbir1257@gmail.com>
2023-09-05 03:12:00 +0600

    add to cart  design section push

---
+++ src/app/(dashboard)/dashboard/user/user_add_to_card/proceed_to_checkout/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProceedToCheckoutMain from '@/components/dashboard/UserDashboard/ProceedToCheckout/ProceedToCheckoutMain';
+
+const ProceedToCheckoutPage = () => {
+    return (
+        <div>
+            <ProceedToCheckoutMain />
+        </div>
+    );
+};
+
+export default ProceedToCheckoutPage;
