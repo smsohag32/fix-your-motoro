0bf4327f091012645b3179d93cf927f67f0f0d68
BidhanCR <bidhanroy300805@gmail.com>
2023-09-07 20:03:22 +0600

    payment method added

--- src/components/dashboard/UserDashboard/ProceedToCheckout/ProceedToCheckoutFetch.jsx
+++ src/components/dashboard/UserDashboard/ProceedToCheckout/ProceedToCheckoutFetch.jsx
@@ -1,29 +1,43 @@
 "use client"
 
 
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import PlaceOrderVoucher from "./PlaceOrderVoucher";
 import Spinner from "@/components/Spinners/Spinner";
+import Image from "next/image";
 
 const ProceedToCheckoutFetch = () => {
   const [cartData, setCartData] = useState([]);
+
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("product")) || [];
     setCartData(data);
-  }, [])
+    console.log(data)
+  }, []);
 
   return (
     <div className="md:mt-16">
-      <h1 className="text-3xl font-bold mb-8">Cart Items</h1>
+      <h1 className="text-3xl font-bold mb-8 text-green-600">Products</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
         <div className="grid grid-cols-1 gap-3">
-        {cartData.map((product, i) => (
-            <div key={product.data._id} className="bg-white shadow-md p-4 rounded-md">
-              <h2 className="text-xl font-semibold">{product.data.productName}</h2>
-              <p className="text-gray-600">Price: ${product.data.price}</p>
-              <p className="text-gray-600">Quantity: {product.data.quantity}</p>
-              <p className="text-gray-600">Total Price: ${product.data.totalPrice}</p>
-              <p className="text-gray-600">Description: {product.data.description}</p>
+          {cartData.map((product, i) => (
+            <div key={product.data._id} className="bg-white shadow-md p-4 rounded-md border border-green-500 ">
+              <div className="flex items-center gap-8">
+                <div>
+                  <Image src={product.data.productImage} alt={product.data.productName} width={150} height={150} className="border border-green-400 hover:border-blue-400" />
+                </div>
+                <div>
+                  <h2 className="text-xl"><span className="font-semibold">Product Name:</span> {product.data.productName}</h2>
+                  <p className="text-gray-600">Price: ${product.data.price}</p>
+                  <p className="text-gray-600">Quantity: {product.data.quantity}</p>
+                  <p className="text-gray-600">
+                    Total Price: <span className="text-red-500 font-bold">
+                      ${product.data.totalPrice}
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <p className="text-gray-600 mt-3">Description: {product.data.description}</p>
             </div>
           ))}
         </div>
