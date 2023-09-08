7b22034a4260d7d27d3eaddd49c03ab32049103d
SM Sohag <sohagsheik32@gmail.com>
2023-09-08 13:04:37 +0600

    service booking and email send error fixing and modal add done

--- src/app/(main)/services/[service_id]/page.js
+++ src/app/(main)/services/[service_id]/page.js
@@ -1,14 +1,28 @@
 "use client";
+import UserModal from "@/components/Modal/Modal";
 import PageTitle from "@/components/Shared/PageTitle/PageTitle";
 import Spinner from "@/components/Spinners/Spinner";
+import useAuth from "@/hooks/useAuth";
 import Image from "next/image";
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
+
+import React, { useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
 import { BiMessageRounded } from "react-icons/bi";
+import Swal from "sweetalert2";
+import emailjs from "@emailjs/browser";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const ServicePage = ({ params }) => {
+  const { user } = useAuth();
   const [service, setService] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const form = useRef();
+  const router = useRouter();
+
+  const { register, handleSubmit, reset } = useForm();
+
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
@@ -43,6 +57,50 @@ const ServicePage = ({ params }) => {
   if (loading) {
     return <Spinner />;
   }
+
+  // booking
+  const onSubmit = async (data) => {
+    const serviceData = {
+      service_id: _id,
+      workshop_email: service?.workshop_email || "sohagsheik32@gmail.com",
+      service_category: service?.service_category,
+      service_image: service?.service_image,
+      service_name: service?.service_name,
+      status: "pending",
+      email: user?.email,
+      ...data,
+    };
+
+    const response = await axios.post(
+      "https://fya-backend.vercel.app/api/v1/auth/orders",
+      serviceData
+    );
+
+    if (response.data) {
+      router.push("/dashboard/user/appointments");
+      // Send Email to user--------ing_
+      try {
+        const response = await emailjs.sendForm(
+          "service_3kn5ji1",
+          "template_zvkyj0s",
+          form.current,
+          "1leqQsJkGshzPjw2s"
+        );
+      } catch (error) {
+        console.error("Email could not be sent:", error);
+      }
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Appointment booking success",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setIsOpen(false);
+      reset();
+    }
+  };
+
   return (
     <div className="mt-32 default-container">
       <PageTitle
@@ -95,9 +153,9 @@ const ServicePage = ({ params }) => {
                 </p>
                 <p className="text-xl text-slate-700">{benefits}</p>
               </div>
-              <Link href={`/services/booking/${_id}`}>
-                <button className="primary-btn">Book Now</button>
-              </Link>
+              <button onClick={() => setIsOpen(true)}>
+                <button className="primary-btn">Appointment Booking</button>
+              </button>
             </div>
           </div>
         </div>
@@ -178,6 +236,235 @@ const ServicePage = ({ params }) => {
           </p>
         </div>
       </div>
+
+      <UserModal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        title={"Booking Request form"}
+      >
+        <div className=" h-full w-full flex items-center justify-center bg-gray-200 bg-opacity-75 z-20">
+          <div className="max-w-5xl  mx-auto w-full bg-white mt-5 md:mt-0 p-6 md:p-12 rounded-lg shadow-lg">
+            <h2 className="text-2xl font-semibold mb-4">Booking Information</h2>
+            <form ref={form} onSubmit={handleSubmit(onSubmit)} className="">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                <div className="col-span-2 sm:col-span-1">
+                  <label
+                    htmlFor="firstName"
+                    className="block text-sm font-medium"
+                  >
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                    required
+                    {...register("firstName")}
+                  />
+                </div>
+                <div className="col-span-2 sm:col-span-1">
+                  <label
+                    htmlFor="lastName"
+                    className="block text-sm font-medium"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                    required
+                    {...register("lastName")}
+                  />
+                </div>
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                <div className="col-span-2 sm:col-span-1">
+                  <label htmlFor="email" className="block text-sm font-medium">
+                    Email
+                  </label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={user?.email}
+                    className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                    placeholder={user?.email}
+                  />
+                </div>
+                <div className="col-span-2 sm:col-span-1">
+                  <label htmlFor="phone" className="block text-sm font-medium">
+                    Phone Number
+                  </label>
+                  <input
+                    type="text"
+                    id="phone"
+                    name="phone"
+                    className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                    placeholder="+8801............"
+                    required
+                    {...register("phone")}
+                  />
+                </div>
+                <div className="col-span-2 sm:col-span-1">
+                  <label
+                    htmlFor="vehicle"
+                    className="block text-sm font-medium"
+                  >
+                    Vehicle Details
+                  </label>
+                  <input
+                    type="text"
+                    id="vehicle"
+                    name="vehicle"
+                    className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                    placeholder="Toyota Land Cruiser Prado - 2020"
+                    required
+                    {...register("vehicle")}
+                  />
+                </div>
+                <div className="col-span-2 sm:col-span-1">
+                  <label
+                    htmlFor="bookingDate"
+                    className="block text-sm font-medium"
+                  >
+                    Date
+                  </label>
+                  <input
+                    type="date"
+                    id="bookingDate"
+                    name="bookingDate"
+                    className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                    required
+                    {...register("bookingDate")}
+                  />
+                </div>
+              </div>
+              <div className="space-y-4 mt-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                  <div>
+                    <label
+                      htmlFor="streetAddress"
+                      className="block text-sm font-medium"
+                    >
+                      Street Address
+                    </label>
+                    <input
+                      type="text"
+                      id="streetAddress"
+                      name="streetAddress"
+                      className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                      {...register("streetAddress")}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="city" className="block text-sm font-medium">
+                      City
+                    </label>
+                    <select
+                      className=" mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                      {...register("city")}
+                    >
+                      <option value="dhaka">dhaka</option>
+                      <option value="khulna">khulna</option>
+                      <option value="chittagong">chittagong</option>
+                      <option value="sylhet">sylhet</option>
+                      <option value="barishal">barishal</option>
+                      <option value="rajshahi">rajshahi</option>
+                      <option value="mymensingh">mymensingh</option>
+                    </select>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="state"
+                      className="block text-sm font-medium"
+                    >
+                      State
+                    </label>
+                    <input
+                      type="text"
+                      id="state"
+                      name="state"
+                      className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                      {...register("state")}
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="postal"
+                      className="block text-sm font-medium"
+                    >
+                      Postal Code
+                    </label>
+                    <input
+                      type="text"
+                      id="postal"
+                      name="postal"
+                      className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                      {...register("postal")}
+                    />
+                  </div>
+                  <div className="">
+                    <label htmlFor="map" className="block text-sm font-medium">
+                      Latitude *
+                    </label>
+                    <input
+                      type="text"
+                      id="map"
+                      name="map"
+                      placeholder="Type your latitude location"
+                      className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                      {...register("user_lat")}
+                    />
+                  </div>
+                  <div className="">
+                    <label htmlFor="long" className="block text-sm font-medium">
+                      Longitude *
+                    </label>
+                    <input
+                      type="text"
+                      id="long"
+                      name="long"
+                      placeholder="Type your longitude location"
+                      className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                      {...register("user_lon")}
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="servicePlace"
+                      className="block text-sm font-medium"
+                    >
+                      Service Place
+                    </label>
+                    <select
+                      className=" mt-1 p-2 w-full border rounded-md focus:outline-none focus:border-sky-500"
+                      {...register("service_type")}
+                    >
+                      <option value="On Garage">On Garage</option>
+                      <option value="On Spot">On Spot</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+              <div className="md:flex justify-between mt-12">
+                <button type="submit" className="primary-btn rounded-md">
+                  Submit
+                </button>
+                <button
+                  onClick={() => setIsOpen(false)}
+                  className="bg-blue-500 text-white px-4 font-semibold tracking-wider py-2 rounded-md hover:bg-blue-600"
+                >
+                  Close
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </UserModal>
     </div>
   );
 };
