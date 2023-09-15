b76df86db9a6d0cfb3bd847ad7bbaa1711c80761
trtonmoy <tr.tonmoy0110.trt@gmal.com>
2023-09-15 13:10:01 +0600

    review dynamic

--- src/components/PagesSection/Home/SuccessReviews/SuccessReviews.jsx
+++ src/components/PagesSection/Home/SuccessReviews/SuccessReviews.jsx
@@ -3,6 +3,7 @@ import StarRating from "./StarRating";
 import SectionTitle from "@/components/Shared/SectionTitle/SectionTitle";
 import Spinner from "@/components/Spinners/Spinner";
 import useReviews from "@/hooks/useReviews";
+import Image from "next/image";
 
 const SuccessReviews = () => {
   const { reviews, rLoading } = useReviews();
@@ -24,9 +25,26 @@ const SuccessReviews = () => {
                 key={review.id}
                 className="bg-white duration-500 hover:scale-x-105 primary-shadow hover-border border-gray-200 border cursor-pointer p-4 rounded-lg"
               >
-                <p className="text-lg font-semibold">{review.user_name}</p>
-                <StarRating rating={review.rating} />
-                <p className="mt-2 text-gray-700">{review.review}</p>
+                <div className="flex items-center gap-5">
+                  <figure>
+                    <Image
+                      className="rounded-full"
+                      src={review.user_img}
+                      alt="User Pic"
+                      height={60}
+                      width={60}
+                    />
+                  </figure>
+                  <div>
+                    <p className="text-lg font-semibold">{review.user_name}</p>
+                    <StarRating rating={review.rating} />
+                    <p className="mt-2 text-gray-700">
+                      {review.review.length > 30
+                        ? review.review.slice(0, 30) + "..."
+                        : review.review}
+                    </p>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
