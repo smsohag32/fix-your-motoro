0c2697ff5d3de808d37f9f77a44a1ffce90cbc02
Tarek Aziz <mdaziz3289@gmail.com>
2023-09-19 02:37:53 +0600

    up

--- src/app/(dashboard)/dashboard/admin/User_review/page.js
+++ src/app/(dashboard)/dashboard/admin/User_review/page.js
@@ -1,7 +1,11 @@
-
+import UserReview from "@/components/dashboard/Admin/UserReview/UserReview";
 
 const UserReviewPage = () => {
-    return <div className="md:my-16">User Review Page</div>;
+  return (
+    <div className="md:my-16">
+      <UserReview/>
+    </div>
+  );
 };
 
 export default UserReviewPage;
